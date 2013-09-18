14c11f9536d3b628e593dccfa141a96527f0d637
Philipp Hausmann <philipp.hausmann@314.ch>
2013-09-18 11:32:25 +0200

    -Improved Countable naming -Added proof/exercise 9.1

--- as1/Afp/As1.hs
+++ as1/Afp/As1.hs
@@ -18,33 +18,44 @@ test = [count, count 1 2 3, count "" [True, False] id (+)]
 test' :: [Int]
 test' = [count', count' 1 2 3, count' "" [True, False] id (+)]
 
-class Count b where
+class Countable b where
   counti :: Int -> b
   counti' :: Int -> b
 
-instance Count Int where
+instance Countable Int where
   counti _ = 0
   counti' = id
 
-instance Count b => Count (a -> b) where
+instance Countable b => Countable (a -> b) where
   counti _ = \x -> counti 0
   counti' acc = \x -> counti' (acc + 1)
 
 -- count always returns zero.
-count :: Count a => a
+count :: Countable a => a
 count = counti 0
 
 -- count' returns the number of arguments given.
-count' :: Count a => a
+count' :: Countable a => a
 count' = counti' 0
 
 
 -- Assignment 7.1
 
+split []     = []
+split (x:xs) = (x,xs):[(y,x:ys) | (y,ys) <- split xs]
 
+perms [] = [[]]
+perms xs = [(v:p) | (v,vs) <- split xs,p <- perms vs]
 
+smooth n (x:y:ys) = abs (y - x) <= n && smooth n (y:ys)
+smooth _ _        = True
+
+smooth_perms :: Int -> [Int] -> [[Int]]
+smooth_perms n xs = filter (smooth n) (perms xs)
 -- Assignment 8.1
 
 
 
 -- Assignment 9.1
+--
+--
