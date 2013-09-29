a3d9c2bfa8953d4c9d03785844ff74aa49540162
Philipp Hausmann <philipp.hausmann@314.ch>
2013-09-29 13:38:56 +0200

    -Finished exercise 2.9

--- as2/src/Afp/As2.hs
+++ as2/src/Afp/As2.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Afp.As2 (
-  dummy,
+  extendedBy,
   -- * Exercise 8.1
   -- $exc81
    
@@ -11,9 +12,7 @@ module Afp.As2 (
 import Test.QuickCheck
 import Control.Monad
 import Control.Monad.Writer
-
-dummy = 1
-
+import Control.Monad.State
 
 
 -- 2.7
@@ -29,25 +28,42 @@ extendedBy o1 o2 super this = o2 (o1 super this) this
 fixObject o = o (error "super") (fixObject o)
 
 
-zero :: Object a
-zero super this = super
-
 x, y, z :: Object X
 x super this = X { n = 0, f = \i -> i + n this }
 y super this = super { n = 1 }
 z super this = super { f = f super . f super }
+
+fac :: Monad m => Object (Int -> m Int)
+fac super this n =
+  case n of
+    0 -> return 1
+    n -> liftM (n*) (this (n - 1))
+
+calls :: MonadState Int m => Object (a -> m b)
+calls super this n =
+  do
+    modify (+1)
+    super n
 
 
 data Step a b = Enter a
   | Return b
   deriving Show
 
-trace :: Object (a -> (b, Writer (Step Int Int) b))
-{--trace super this arg = pass $ do
-  tell (Enter 0)
-  super arg
---}
-trace = undefined
+
+-- Solution Task 2.7.1
+
+zero :: Object a
+zero super this = super
+
+-- Solution Task 2.7.2
+
+trace :: MonadWriter [Step a b] m => Object (a -> m b)
+trace super this arg = do
+  tell [Enter arg]
+  res <- super arg
+  tell [Return res]
+  return res
 
 
 -- 2.9
@@ -58,6 +74,8 @@ p1 = start store 3 store 5 add stop
 p2 = start store 3 store 6 store 2 mul add stop
 --p3 = start store 2 add stop
 
+
+-- Solution Task 2.9
 data Zero
 data Succ a
 
