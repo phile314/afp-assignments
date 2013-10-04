fd8ecb4d2578f34ae93194882d43157701ea83c0
Laurens <laurensvdbrink@hotmail.com>
2013-10-04 18:29:05 +0200

    3 monads + comment

--- as2/src/Afp/As2.hs
+++ as2/src/Afp/As2.hs
@@ -13,6 +13,12 @@ module Afp.As2 (
   start, stop, store, add, mul, nop,
   Stack, Cont, Zero1, Succ1,
 
+  -- * Task 3
+  StateMonadPlus, Entry, 
+  diagnostics, annotate,
+  evalStatePlus,
+  StoreState(..),
+
   -- * Task 4.1
   y',
 
@@ -26,6 +32,7 @@ module Afp.As2 (
 import Control.Monad
 import Control.Monad.Writer
 import Control.Monad.State
+import Control.Monad.Error
 
 
 -- 2.7
@@ -129,14 +136,20 @@ mul (St (s1:s2:st)) = \c -> c (St ((s1 * s2):st))
 -- | Does nothing.
 nop :: Stack a -> Cont a b -> b
 nop s = \c -> c s
+
 -- 3
 --
+
+-- Datatypes to track the state alongside of our state
 type MyState = [Entry]
-data Entry = Entry (String, Int)
+data Entry = Entry (String, Int) | Store [Int]
 
 instance Show Entry where
 	show (Entry (s, i)) = s ++ "=" ++ show i
+	show (Store s) 		= "store=" ++ show s
 
+-- Custom datatype for State Monad
+-- | The StateMonadPlus provides a state in alongside of the MyState
 data StateMonadPlus s a = SMP (State (MyState, s) a)
 
 instance Monad (StateMonadPlus s) where  
@@ -150,81 +163,140 @@ instance MonadState s (StateMonadPlus s) where
     put s = SMP $ state $ \(m,_) -> ((), (addPut m,s))
 
 
-evalStatePlus :: StateMonadPlus s a -> (MyState, s) -> a
-evalStatePlus (SMP act) = fst . runState act
-
+-- update in the custom state that return is called
 addReturn :: MyState -> MyState
 addReturn = update "return"
 
+-- update in the custom state that put is called
 addPut :: MyState -> MyState
 addPut = update "put"
 
+-- update in the custom state that get is called
 addGet :: MyState -> MyState
 addGet = update "get"
 
+-- update in the custom state that bind is called
 addBind :: MyState -> MyState
 addBind = update "bind" 
 
+-- update in the custom state that diagnostics is called
 addDiagnostics :: MyState -> MyState
 addDiagnostics = update "diagnostics"
 
+-- update in the custom state that annotate is called
+-- update in one go the annotate and the annotation
 addAnnotate :: String -> MyState -> MyState
 addAnnotate = update False
 	where
-		update _ a [] = [Entry ("annotate", 1), Entry (a, 1)]
+		update _ a [] 									= [Entry ("annotate", 1), Entry (a, 1)]
 		update b a (Entry (s, i):xs)| s == "annotate" 	= Entry (s, i+1) : (if b then xs else update True a xs)
 									| s == a 			= Entry (s, i+1) : (if b then xs else update True a xs)
 									| otherwise			= Entry (s, i) : update b a xs
+		update b a (x:xs)								= x: update b a xs
 
+-- update an entry in the custom state
 update :: String -> MyState -> MyState
-update a [] 							= [Entry (a, 1)]
-update a (Entry (k,v):xs) 	| k == a 	= Entry (k, v+1) : xs
-							| otherwise = Entry (k,v) : update a xs
+update a = updateEntry a (1+)
 
+updateEntry :: String -> (Int -> Int) -> MyState -> MyState
+updateEntry a _ [] 					= [Entry (a, 1)]
+updateEntry a f (Entry (k,v):xs)
+	| k == a						= Entry (k, f v) : xs
+	| otherwise						= Entry (k, v) : updateEntry a f xs
+updateEntry a f (x:xs)				= x : updateEntry a f xs
+
+
+-- | The function diagnostics substitutes the value of the state with the output of the custom state
 diagnostics :: StateMonadPlus s String
 diagnostics = SMP $ state $ \(m,s) -> ((show $ addDiagnostics m), (addDiagnostics m,s))
 
+
+-- | The function annotate let you update a value into the custom state and continues with the next execution
 annotate :: String -> StateMonadPlus s a -> StateMonadPlus s a
 annotate a (SMP x) = SMP $ state $ \(m,s) -> runState x (addAnnotate a m, s)
 
+-- monadDiagnostics is the function provided in the exercise for testing the diagnostics function
 monadDiagnostics :: StateMonadPlus Int String
 monadDiagnostics = 
 	do 	return 3 >> return 4
 		return 5
 		diagnostics
 
+-- monadAnnotate is the function provided in the exercise for testing the annotate function
 monadAnnotate :: StateMonadPlus Int String
 monadAnnotate =
 	do	annotate "A" (return 3 >> return 4)
 		return 5
 		diagnostics
 
-startStatePlus :: (MyState, Int)
-startStatePlus = ([], 0)
+-- let you evalute a StateMonadPlus provided a state
+evalStatePlus :: StateMonadPlus s a -> (MyState, s) -> a
+evalStatePlus (SMP act) = fst . runState act
 
-testDiagnostics = print $ evalStatePlus monadDiagnostics startStatePlus
-testAnnotate = print $ evalStatePlus monadAnnotate startStatePlus
+-- initalization of a state for StateMonadPlus
+initState :: (MyState, Int)
+initState = ([], 1)
+
+-- function to test the functionality
+testDiagnostics = print $ evalStatePlus monadDiagnostics initState
+testAnnotate = print $ evalStatePlus monadAnnotate initState
 
 
+--
 class MonadState s m => StoreState s m | m -> s where
 	saveState :: m ()
 	loadState :: m ()
 
-fail :: (Monad m) => String -> m a
-fail = error
+instance StoreState Int (StateMonadPlus Int) where
+	saveState = SMP $ state $ \(m,s) -> ((), (pushStack s m, s))
+	loadState = SMP $ state $ \(m,s) -> case catchError (popStack m) fail of
+		(Right (i, m'))-> ((), (m',i))
 
-runStateMonadPlus :: StateMonadPlus s a -> s -> Either String (a, s)
-runStateMonadPlus = undefined
+-- failing function uses the functionality from the error function
+failing :: (Monad m) => String -> m a
+failing = error
 
-test = 
-	do	i1 <- get; saveState
-		modify (*2)
-		i2 <- get; saveState
-		modify (*2); saveState
-		i3 <- get; loadState
-		i4 <- get; loadState
-		i5 <- get
-		return (i1,i2,i3,i4,i5)
+-- push a value on the stack inside the custom state
+pushStack :: Int -> MyState -> MyState
+pushStack i [] = [Store [i]]
+pushStack i (Store s:xs) = Store (i:s):xs
+pushStack i (x:xs) = x : pushStack i xs
+
+-- pops a value from the stack inside the custom state
+-- returns the value and the changed state 
+popStack :: MyState -> Either String (Int, MyState)
+popStack = pop []
+	where 
+		pop _ []				= failing "failed to pop from the stack"
+		pop _ (Store []:_) 		= failing "failed to pop from the stack"
+		pop m (Store (y:ys):xs)	= Right (y, m ++ (Store ys : xs))
+		pop m (x:xs) 			= pop (x:m) xs
+
+-- tests the functionality of the StoreState monad
+testStore = print $ evalStatePlus test initState
+	where test = 
+			do	i1 <- get; saveState
+				modify (*2)
+				i2 <- get; saveState
+				modify (*2)
+				i3 <- get; loadState
+				i4 <- get; loadState
+				i5 <- get
+				return (i1,i2,i3,i4,i5)
+
+-- tests the functionality of the StoreState monad
+-- the test is designed to fail, because it will try to load a value from a empty stack
+testStoreFail = print $ evalStatePlus test initState
+	where test = 
+			do	i1 <- get; loadState
+				modify (*2)	
+				i2 <- get; saveState
+				modify (*2)
+				i3 <- get; loadState
+				i4 <- get; loadState
+				i5 <- get
+				return (i1,i2,i3,i4,i5)
+
 
 -- 4.1
 --
