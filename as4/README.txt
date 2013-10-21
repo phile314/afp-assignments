51add799459889f7960f30a723398f44db60e08b
Philipp Hausmann <philipp.hausmann@314.ch>
2013-10-21 13:52:20 +0200

    Added assignment4 skeleton

---
+++ as4/README.txt
@@ -0,0 +1,22 @@
+-- Who --
+
+Philipp Hausmann,      4003373, p.hausmann@students.uu.nl
+Laurens van den Brink, 4022831, l.vandenbrink1@students.uu.nl
+
+
+
+
+
+-- What's Where --
+everything -> src/Afp/As4.hs
+
+
+-- Building --
+
+
+$ cabal configure
+$ cabal build
+
+
+-- References --
+
---
+++ as4/src/Afp/As4.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+
+module Afp.As4
+  ( -- * Task 10.3
+
+    -- * Task 11.1
+
+    -- * Task 11.2
+  )
+where
+
+-- 10.3
+-- ph
+--
+-- TODO
+
+
+
+-- 11.1
+--
+--
+
+
+-- 11.2
