f244fb665205d1518c7efdff01897973bd80a1e7
Laurens <laurensvdbrink@hotmail.com>
2013-10-30 14:26:53 +0100

    11.1 only the quickcheck still to do..

--- as4/src/Afp/As4.hs
+++ as4/src/Afp/As4.hs
@@ -1,11 +1,3 @@
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE TypeSynonymInstances #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE MultiParamTypeClasses  #-}
-
 module Afp.As4
   ( -- * Task 10.3
 
@@ -14,6 +6,8 @@ module Afp.As4
     -- * Task 11.2
   )
 where
+
+import Data.Generics
 
 -- 10.3
 -- ph
@@ -25,6 +19,21 @@ where
 -- 11.1
 --
 --
+
+-- | A show function for a datatype using the syb library
+gshowx :: (Data a) => a -> String
+gshowx = (\x -> "(" ++ showConstr (toConstr x) ++
+		concat (gmapQ ((++) " " . gshowx) x) ++ ")"
+	) `extQ` (show :: String -> String)
+
+
+-- | A read function for a datatype using the syb library
+greadx :: Data a => String -> a
+greadx s = either error id (readEither' s)
+	where readEither' s = case [x | (x, _) <- gread s] of 
+		(x:_)	-> Right x
+		_		-> Left "read: failed to parse"
+
 
 
 -- 11.2
