f3f75f39a71d9e3c0e802045c22206ffacac99e8
Laurens <laurensvdbrink@hotmail.com>
2013-11-01 16:34:35 +0100

    11.1 and start of 11.2

--- as4/src/Afp/As4.hs
+++ as4/src/Afp/As4.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeOperators, FlexibleInstances #-}
 module Afp.As4
   ( -- * Task 11.1
 
@@ -6,6 +7,11 @@ module Afp.As4
 where
 
 import Data.Generics
+import qualified Generics.Deriving as D
+import Control.Monad
+import Text.Read
+import Data.Char
+import Test.QuickCheck
 
 
 -- 11.1
@@ -26,6 +32,40 @@ greadx s = either error id (readEither' s)
 		(x:_)	-> Right x
 		_		-> Left "read: failed to parse"
 
+-- | Test for the property of show and read: read . show == id
+prop_ShowRead :: (Eq a, Data a) => a -> Bool
+prop_ShowRead x = x == (greadx $ gshowx x) 
 
 
 -- 11.2
+
+
+class DRead f where
+   hreader :: ReadPrec a -> Bool -> ReadPrec (f a)
+
+instance DRead D.U1 where
+   hreader _ _ = return D.U1
+
+instance (Read a) => DRead (D.K1 i a) where
+   hreader _ _ = liftM D.K1 (readS_to_Prec readsPrec)
+
+instance (DRead f, DRead g) => DRead (f D.:+: g) where
+   hreader f r = liftM D.L1 (hreader f r) +++ liftM D.R1 (hreader f r)
+
+instance (DRead f, DRead g) => DRead (f D.:*: g) where
+   hreader f r = do l' <- hreader f r
+                    when r $ do Punc "," <- lexP
+                                return ()
+                    r' <- hreader f r
+                    return (l' D.:*: r')
+
+instance (D.Constructor c) => DRead (D.M1 D.C c a) where
+	hreader f _ = undefined
+instance (D.Constructor c) => DRead (D.M1 D.S c a) where
+	hreader f _ = undefined
+
+-- dRead :: (D.Generic a, DRead (D.Rep a)) => String -> a
+dRead s = case [x |  (x,remain) <- readsPrec 0 s , all isSpace remain] of
+           [x] -> x 
+           [ ] -> error "no parse"
+           _   -> error "ambiguous parse"
